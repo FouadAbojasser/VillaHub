d75f53b7c869c9c0e910926bc1bb42d5272a0c3a
fouadabujasser <fouad.abujasser@gmail.com>
2025-06-21 15:33:00 +0300

    Add project files.

---
+++ VillaHub.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VillaHub.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
