1c406569fcac40eca0478fd4bdf232a98ce270c3
fouadabujasser <fouad.abujasser@gmail.com>
2025-08-27 19:37:59 +0300

    Add Delete Review and more improvments

--- VillaHub.Web/Controllers/ReviewController.cs
+++ VillaHub.Web/Controllers/ReviewController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using VillaHub.Domain.Entities;
 using VillaHub.Application.Common.Interfaces;
 using VillaHub.Application.Common.Utility;
 
 namespace VillaHub.Web.Controllers
 {
-    
+
     [Authorize(Roles = SD.Role_SuperAdmin)]
     public class ReviewController : Controller
     {
@@ -17,22 +18,22 @@ namespace VillaHub.Web.Controllers
 
         public IActionResult Index()
         {
-            var AllReviews = _unitOfWork.Review.Get(null, [e=>e.Floor.Villa, e=>e.Floor.Village]);
-           
+            var AllReviews = _unitOfWork.Review.Get(null, [e => e.Floor.Villa, e => e.Floor.Village]);
+
             return View(AllReviews);
         }
 
 
-        public async Task<IActionResult> AddReplyAsync (int Id, string ReplyText, string RepliedBy)
+        public async Task<IActionResult> AddReplyAsync(int Id, string ReplyText, string RepliedBy)
         {
             var userCommentInDB = _unitOfWork.Review.GetOne(e => e.Id == Id);
 
-            if(userCommentInDB is null)
+            if (userCommentInDB is null)
             {
                 return BadRequest();
             }
 
-            if(!string.IsNullOrEmpty(ReplyText) && !string.IsNullOrEmpty(RepliedBy))
+            if (!string.IsNullOrEmpty(ReplyText) && !string.IsNullOrEmpty(RepliedBy))
             {
                 userCommentInDB.Reply = ReplyText;
                 userCommentInDB.ReplyUserId = RepliedBy;
@@ -41,9 +42,38 @@ namespace VillaHub.Web.Controllers
                 _unitOfWork.Review.Update(userCommentInDB);
                 await _unitOfWork.Review.CommitAsync();
             }
-            
 
             return RedirectToAction(nameof(Index));
         }
+
+
+        public IActionResult Delete(Review review)
+        {
+            var reviewInDb = _unitOfWork.Review.GetOne(e=>e.Id==review.Id, [e => e.Floor.Villa, e => e.Floor.Village]);
+
+            if (reviewInDb is null)
+            {
+                return BadRequest();
+            }
+
+            return View(reviewInDb);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int Id)
+        {
+            var reviewInDb = _unitOfWork.Review.GetOne(e => e.Id == Id);
+            if (reviewInDb is null)
+            {
+                return BadRequest();
+            }
+            _unitOfWork.Review.Delete(reviewInDb);
+            _unitOfWork.Review.CommitAsync();
+
+            TempData["success"] = "Review Deleted Successfully";
+            return RedirectToAction(nameof(Index));
+
+        }
+
     }
 }
